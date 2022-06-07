5b908e8a9375d3b7334feeca92c329ac93582cfe
Anshul Pandey <54087991+Anshul275@users.noreply.github.com>
2022-06-07 07:27:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,8 +27,7 @@ mongoose.connect(
 	process.env.DB_CONNECT,
 	{
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
+		useUnifiedTopology: true
 	},
 	() => console.log("Connected to DB!")
 );
